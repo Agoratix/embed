91ae8c8a28b3c04aa222afaba9d8c697ce39ccfc
Victor Abecassis <v.abecassis@yahoo.com>
2023-01-16 21:01:26 -0800

    io

--- src/types/config.ts
+++ src/types/config.ts
@@ -43,7 +43,7 @@ export interface CrossmintEmbedConfig {
 
 export enum CrossmintEnvironment {
     PROD = "https://www.crossmint.com",
-    STAGING = "https://staging.crossmint.com",
+    STAGING = "https://staging.crossmint.io",
     LOCAL = "http://localhost:3001",
 }
 
